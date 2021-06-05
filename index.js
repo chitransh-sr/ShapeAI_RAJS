cb7351db4098300ac18129fb376eaee75a20994a
Chitransh Srivastava <73604474+chitransh-sr@users.noreply.github.com>
2021-06-05 14:34:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Heading from "../Public/components/header";  
+import List from "../Public/components/list";
+ReactDOM.render(
+  <div>
+    <Heading/>
+    <List/>
+  </div>,
+  document.getElementById("root")
+);
