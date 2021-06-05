a3518f6ed67016567c1fd861550dbd6ed2cccbca
Chitransh Srivastava <73604474+chitransh-sr@users.noreply.github.com>
2021-06-05 14:26:26 +0530

    Add files via upload

---
+++ list.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function List() {
+    <ul className="list">
+        <li> The instructor during the session was Mr. Shaurya Sinha (Data Analyst Intern at Jio). I got to learn a lot during these 7 days and it was an amazing experience learning with SHAPEAI.</li>
+    </ul>
+
+
+}
+
+export default List;
