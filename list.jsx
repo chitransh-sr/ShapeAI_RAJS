46bc55998cd8ef29b68b749c77235ffbdfa7de7f
Chitransh Srivastava <73604474+chitransh-sr@users.noreply.github.com>
2021-06-05 14:32:58 +0530

    Add files via upload

--- list.jsx
+++ list.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 function List() {
+    return (
     <ul className="list">
+        <li>Hi My name is Chitransh Srivastava</li>
         <li> The instructor during the session was Mr. Shaurya Sinha (Data Analyst Intern at Jio). I got to learn a lot during these 7 days and it was an amazing experience learning with SHAPEAI.</li>
     </ul>
-
+    );
 
 }
 
